cd24debcfabdfe5c12f6e7f757c2263710455f07
EternalCoders <shivamchoudhary602@gmail.com>
2026-01-06 18:33:58 +0530

    code: Working on the Brushing up old concepts,
    class based views -|
                       |- ViewSets - (viewsets.ViewSet - list , create, retrieve, update, delete)

--- api/views.py
+++ api/views.py
@@ -191,20 +191,42 @@ class EmployeesDetail(generics.RetrieveAPIView, generics.UpdateAPIView , generic
 
 class EmployeeViewSet(viewsets.ViewSet):
 
-    def list(self, request):
+    def list(self , request):
         queryset = Employee.objects.all()
         serializer = EmployeeSerializer(queryset, many = True)
         return Response(serializer.data)
     
+
     def create( self, request):
         serializer = EmployeeSerializer(data = request.data)
+
         if serializer.is_valid():
+
             serializer.save()
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
     def retrieve(self, request, pk=None):
         employee = get_object_or_404(Employee, pk=pk)
         serializer = EmployeeSerializer(employee)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
 
+    def update(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee, data= request.data)
+
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_304_NOT_MODIFIED)
+
+
+    def delete(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        employee.delete()
+        return Response(status=status.HTTP_404_NOT_FOUND)
